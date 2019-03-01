fc20cc51db9c8d2db4f54e4eb6042e59b77d0fb3
tahirdevelop <tahirdevelop@gmail.com>
2019-03-01 02:23:20 +0200

    v2

--- form_app/form.py
+++ form_app/form.py
@@ -3,19 +3,21 @@ from django import forms
 
 class Form_demo(forms.Form):
 
-    fullName = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Имя',
-                                                                             'class': 'form-control '}))
+    fullName = forms.CharField(max_length=150, widget=forms.TextInput(
+        attrs={'placeholder': 'Имя', 'class': 'form-control', 'id': 'id_fullName'}))
 
-    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'E-mail', 'class': 'form-control'}))
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'E-mail', 'class': 'form-control', 'id': 'id_email'}))
 
-    phoneNumber = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Номер телефона',
-                                                                               'class': 'form-control'}))
+    phoneNumber = forms.CharField(max_length=15, widget=forms.TextInput(
+        attrs={'placeholder': 'Номер телефона', 'class': 'form-control', 'id': 'id_phoneNumber'}))
 
-    company = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Компания',
-                                                                           'class': 'form-control'}))
+    company = forms.CharField(max_length=30, widget=forms.TextInput(
+        attrs={'placeholder': 'Компания', 'class': 'form-control', 'id': 'id_company'}))
 
-    message = forms.CharField(max_length=200, widget=forms.Textarea(attrs={'placeholder': 'Сообщение',
-                                                                            'class': 'form-control', 'rows': 3}))
+    message = forms.CharField(max_length=200, widget=forms.Textarea(
+        attrs={'placeholder': 'Сообщение', 'class': 'form-control', 'rows': 3, 'id': 'id_message'}))
+
     def __str__(self):
         return f"Имя: {self.cleaned_data['fullName']} \n" \
                f"E-mail: {self.cleaned_data['email']} \n" \
--- form_app/views.py
+++ form_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render , redirect
 from django.views.generic import View
 from .form import Form_demo
 import asana
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt , csrf_protect
+
+
 # Create your views here.
 
 
@@ -9,13 +14,13 @@ class Index(View):
 
     def get(self, request):
         form = Form_demo()
-        return render(request, template_name='form_app/index.html', context={'form': form})
+        return render(request, template_name='form_app/index.html', context={'form': form} )
 
     def post(self, request):
         form = Form_demo(request.POST)
         if form.is_valid():
-            client = asana.Client.access_token('0/e53079d9dab14077f642629b927a9ece')
-            client.tasks.create_in_workspace( workspace='1111813369839902', projects='1111842186512584',
-                                              name=form.cleaned_data['company'],
-                                              notes=form.__str__())
-            return redirect()
+            client = asana.Client.access_token( '0/e53079d9dab14077f642629b927a9ece' )
+            client.tasks.create_in_workspace(workspace='1111813369839902' , projects='1111842186512584',
+                                             name=form.cleaned_data['company'], notes=form.__str__())
+
+        return HttpResponse('200')
