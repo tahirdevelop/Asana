89aeef90c890b52bb013754f2921c87938c069be
tahirdevelop <tahirdevelop@gmail.com>
2019-03-01 02:45:23 +0200

    v2.1

---
+++ form_app/static/js/submit.js
@@ -0,0 +1,51 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
+$(document).ready(function () {
+
+    $('.submit').bind('click', function () {
+        $.ajax({
+            url: "",
+            type: "POST",
+            dataType: 'json',
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'fullName': $('#id_fullName').val(),
+                'email': $('#id_email').val(),
+                'phoneNumber': $('#id_phoneNumber').val(),
+                'company': $('#id_company').val(),
+                'message': $('#id_message').val()
+            }
+        });
+    });
+
+    $(".submit").click(function () {
+        $(".submit").addClass("loading");
+        setTimeout(function () {
+            $(".submit").addClass("hide-loading");
+            // For failed icon just replace ".done" with ".failed"
+            $(".done").addClass("finish");
+        }, 1000);
+        setTimeout(function () {
+            $(".submit").removeClass("loading");
+            $(".submit").removeClass("hide-loading");
+            $(".done").removeClass("finish");
+            $(".failed").removeClass("finish");
+        }, 2500);
+    })
+});
